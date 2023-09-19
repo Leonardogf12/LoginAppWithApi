46cb349102c066f2487471148c0dcbcaf474fdeb
Leonardogf12 <73491267+Leonardogf12@users.noreply.github.com>
2023-09-19 20:51:24 -0300

    First Commit
    First Commit

---
+++ LoginApp/LoginApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LoginApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
